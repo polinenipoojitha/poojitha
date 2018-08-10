0e2c28698468e00afda097a04d1f5e3f8313065c
polinenipoojitha <41770744+polinenipoojitha@users.noreply.github.com>
2018-08-10 11:09:18 +0530

    Update program14.py

--- program14.py
+++ program14.py
@@ -1,7 +1,7 @@
 lower = 1
 upper = 6
 print("prime numbers between",lower,"and",upper,"are:")
-for num in range(lower,upper+1):
+for num in range(lower,upper + 1):
 if num > 1:
 for i in range(2,num):
 if(num % i == 0):
