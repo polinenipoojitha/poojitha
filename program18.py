2cb8aef8a2fbcc14150406f0cef0bc8d4f50cd61
polinenipoojitha <41770744+polinenipoojitha@users.noreply.github.com>
2018-08-11 14:40:41 +0530

    Create program18.py

---
+++ program18.py
@@ -0,0 +1,12 @@
+lower = 100
+upper = 200
+for num in range(lower,upper + 1):
+	order = len(str(sum))
+	sum = 0
+	temp = num
+	while temp > 0:
+		digit = temp % 10
+		sum += digit ** order
+		temp //= 10
+		if num == sum:
+			print(num)
