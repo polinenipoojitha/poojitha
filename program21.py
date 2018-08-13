c47a4af215a8f715fe5af2dcccdab303161150fb
polinenipoojitha <41770744+polinenipoojitha@users.noreply.github.com>
2018-08-13 11:46:03 +0530

    Create program21.py

---
+++ program21.py
@@ -0,0 +1,12 @@
+def sumofAP(a, d, n):
+    sum = 0
+    i = 0
+    while i < n:
+          sum = sum + a
+          a = a + d
+          i = i + 1
+    return sum
+n = 3
+a = 1
+d = 1
+print(sumofAP(a, d, n))
