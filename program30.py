7cfb4c35fb5aeb57570431b897aecb1ed00d0f1e
polinenipoojitha <41770744+polinenipoojitha@users.noreply.github.com>
2018-08-16 15:02:02 +0530

    Update program30.py

--- program30.py
+++ program30.py
@@ -3,6 +3,6 @@ hr2,min2=map(int,raw_input().split())
 n1=hr1*60+min1
 n2=hr2*60+min2
 diff=abs(n1-n2)
-time=diff%60
-time1=(diff-time)//60
-print time1,time
+time1=diff%60
+time2=(diff-time)//60
+print time1,time2
